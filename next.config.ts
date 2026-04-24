be6d0baa8827dd5f8a71f74420d8d1fc1d26732d
Tatjana Devrnja <devrnjat94@gmail.com>
2026-04-24 21:52:56 +0200

    Setup base app structure and config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'testapp.local',
+        pathname: '/**',
+      },
+    ],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
